323cc1383c1674bc54631dcc9a4d7b7a965e2ecd
Jxjdjjdfhdhd <oppolm1020@gmail.com>
2024-08-28 09:59:15 +0400

    Update info.py

--- info.py
+++ info.py
@@ -27,8 +27,8 @@ PICS = (environ.get('PICS', 'https://telegra.ph/file/5de2a87af196bcd866b7f.jpg')
 NOR_IMG = environ.get("NOR_IMG", "https://telegra.ph/file/5de2a87af196bcd866b7f.jpg")
 
 # Admins, Channels & Users
-OWNER_ID = environ.get('OWNER_ID', '')
-ADMINS = [int(admin) if id_pattern.search(admin) else admin for admin in environ.get('ADMINS', '1297128957 1509123054').split()]
+OWNER_ID = environ.get('OWNER_ID', '7040444713')
+ADMINS = [int(admin) if id_pattern.search(admin) else admin for admin in environ.get('ADMINS', '7040444713').split()]
 CHANNELS = [int(ch) if id_pattern.search(ch) else ch for ch in environ.get('CHANNELS', '-1002208570147').split()]
 auth_users = [int(user) if id_pattern.search(user) else user for user in environ.get('AUTH_USERS', '').split()]
 AUTH_USERS = (auth_users + ADMINS) if auth_users else []
